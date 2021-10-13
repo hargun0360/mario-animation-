6c1b1feff8e28b48752f8753c1064db6b7b7d52c
=Hargun Singh <=Hargun0360@gmail.com>
2021-10-13 17:16:43 +0530

    add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-var img = ['../images/mario3.png', '../images/mario1.png', '../images/mario2.png']
+var img = ['./images/mario3.png', './images/mario1.png', './images/mario2.png']
 var i=0;
 
 
@@ -14,44 +14,39 @@ var i=0;
 
 
 var pos= -150;
-var speed=10;
-var e = document.getElementById("obstacle");
- var m=window.getComputedStyle(e);   
+var speed=15;
+var e = document.getElementById("obstacle"); 
 setInterval(function dragon(){
         if(pos==window.innerWidth){
         pos=0;
         }
         else{
         e.style.marginLeft=((800) -pos )+ "px";
-        console.log(e.style.marginLeft);
+        // console.log(e.style.marginLeft);
         pos++;
         if((e.style.marginLeft < 100+"px" )){
             alert("game over");
     
         }
     }
-},speed--);
+},speed);
 
         
-document.onkeydown=function(e){
-    if(e.keycode==38){
-        let mario=document.querySelector('#mario_img');
-        mario.classList.add('.animatemario');
-        setTimeout(() => {
-            mario.classList.remove('.animatemario');
-        }, 800);
-    }
-}
+// document.onkeydown=function(e){
+//     if(e.keycode==38){
+//         let mario=document.querySelector('#mario_img');
+//         mario.classList.add('.animatemario');
+//         setTimeout(() => {
+//             mario.classList.remove('.animatemario');
+//         }, 800);
+//     }
+// }
 document.onkeydown = jump;
 var mario=document.querySelector("#mario");
 function jump(e){
-    // var pleft=document.querySelector("#mario").offsetLeft;
     if(e.keyCode==32)
     {
-        if(mario.classList=="animat")
-        {
-            return
-        }
+        
         mario.classList.add("animat");
         setTimeout(function(){
             mario.classList.remove("animat");
